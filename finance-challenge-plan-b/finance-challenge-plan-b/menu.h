a3b221c6c8857f786785610636d2c72d71b366ff
SDDimitrov22 <SDDimitrov22@codingburgas.bg>
2024-11-02 14:15:41 +0200

    Add void MonthlyInclomeandExpenses

--- finance-challenge-plan-b/finance-challenge-plan-b/menu.h
+++ finance-challenge-plan-b/finance-challenge-plan-b/menu.h
@@ -1,1 +1,7 @@
 #pragma once
+#include  <iostream>
+#include <string>
+
+using namespace std;
+static int choice;
+void menu(int);
--- finance-challenge-plan-b/finance-challenge-plan-b/options.cpp
+++ finance-challenge-plan-b/finance-challenge-plan-b/options.cpp
@@ -1,2 +1,7 @@
 #include "../finance-challenge-plan-b/menu.h"
 #include "../finance-challenge-plan-b/options.h"
+
+void MonthlyInclomeandExpenses()
+{
+
+}
